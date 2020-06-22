668a82912da25c59667e92919790dda8c03bb401
larisachristie <larisachristie@gmail.com>
2020-06-22 19:04:36 +0200

    operative functions redone

--- index.js
+++ index.js
@@ -1,70 +1,66 @@
 let input1;
 let input2;
 let operator;
+
+const display = value => {
+  document.getElementById('output').textContent = value;
+}
 const reset = () => {
   input1 = input2 = operator = undefined;
+  display("0");
 }
-const clearDisplay = () => {
-  document.getElementById('output').innerHTML = "0";
-}
-const displayInput = value => {
-  document.getElementById('output').innerHTML = value;
-}
+
 const setInput = value => {
-  displayInput(value);
   if (input1) {
     if (!operator) {
-      console.log(typeof input1);
-      input1 = Number(input1 + value);
+      input1 = input1 + value;
     }
-    input2 = Number(input2 + value);
+    input2 = input2 + value;
   } else {
-    input1 = Number(value);
+    input1 = value;
   }
+  display(value);
 };
 
-
+const setOperator = value => {
+  operator = value;
+};
 
 const add = (a, b) => {
-  operator = 'addition';
-  return a + b;
+  return Number(a) + Number(b);
 };
 
 const substract = (a, b) => {
-  operator = 'substract';
-  return a - b;
+  return Number(a) - Number(b);
 };
 
 const multiply = (a, b) => {
-  operator = 'multiply';
-  return a * b;
+  return Number(a) * Number(b);
 };
 
 const divide = (a, b) => {
   if (!b == 0) {
-    operator = 'divide';
-    return a / b;
+    return Number(a) / Number(b);
   } else {
-    console.log('don\'t divide by zero, dummy!' )
+    display('don\'t divide by zero, dummy!');
   }
 };
 
 const equal = () => {
   switch (operator) {
-    case 'addition':
-      document.getElementById('output').innerHTML = add(input1, input2);
+    case 'add':
+      display(add(input1, input2));
       break;
     case 'substract':
-      document.getElementById('output').innerHTML = substract(input1, input2);
+      display(substract(input1, input2));
       break;
     case 'multiply':
-      document.getElementById('output').innerHTML = multiply(input1, input2);
+      display(multiply(input1, input2));
       break;
     case 'divide':
-      document.getElementById('output').innerHTML = divide(input1, input2);
+      display(divide(input1, input2));
       break;
     default:
       break;
   }
-  reset();
 };
